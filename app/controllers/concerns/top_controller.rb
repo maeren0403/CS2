b41df0375e1454919bd1741193b2983b13f7ae1f
maeren0403 <2211141001s@ed.fuk.kindai.ac.jp>
2022-10-11 12:10:00 +0000

    CS2を編集

---
+++ app/controllers/concerns/top_controller.rb
@@ -0,0 +1,26 @@
+class TopController < ApplicationController
+    def main
+        if session[:login_uid]
+           session[:login_pass]
+            render "main"
+        else
+            render "login"
+        end
+    end
+   
+    def login
+       if User.find_by(uid: params[:uid], pass: params[:pass])
+           session[:login_uid] = params[:uid]
+ 
+           redirect_to root_path
+       else
+            render 'error'
+       end
+    end
+    
+    def logout
+        session.delete(:login_uid)
+        session.delete(:login_pass)
+        redirect_to root_path
+    end
+end
